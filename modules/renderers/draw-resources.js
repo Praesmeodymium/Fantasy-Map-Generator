ceaf9cf7066f84231914b800ebd7fe1538462a18
Praesmeodymium <lone_cyclist@yahoo.com>
2025-07-20 18:27:51 -0700

    Add resource editing and deposit size

--- modules/renderers/draw-resources.js
+++ modules/renderers/draw-resources.js
@@ -3,10 +3,11 @@
 function drawResources() {
   TIME && console.time("drawResources");
   resources.selectAll("*").remove();
+  const bySize = Resources.getDisplayMode();
   const html = pack.resources.map(r => {
     const type = Resources.getType(r.type);
     const color = type?.color || "#000";
-    const size = 3;
+    const size = bySize ? (r.size || 1) * 3 : 3;
     const name = type?.name || "Unknown";
     return `<circle id="resource${r.i}" cx="${r.x}" cy="${r.y}" r="${size}" fill="${color}" data-tip="${name}" />`;
   });
