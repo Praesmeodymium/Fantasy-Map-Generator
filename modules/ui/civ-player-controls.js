a41e2783dfb2e64a7c7591c754fffb65e00308cc
Praesmeodymium <lone_cyclist@yahoo.com>
2025-07-26 11:13:32 -0700

    Revise civilization player controls

--- modules/ui/civ-player-controls.js
+++ modules/ui/civ-player-controls.js
@@ -1,19 +1,47 @@
-"use strict";
+'use strict';
 
 window.CivPlayerControls = (() => {
-  function init() {
-    const container = document.getElementById("civPlayerControls");
-    if (!container) return;
-    container.innerHTML = `
-      <button id="civPlayBtn">Play</button>
-      <button id="civPauseBtn">Pause</button>
-      <button id="civStepBackBtn">Step -</button>
-      <button id="civStepForwardBtn">Step +</button>
-    `;
-    container.style.display = "block";
+  let playing = false;
+
+  function showLayer() {
+    if (!layerIsOn('toggleStates')) toggleStates();
   }
 
-  return {init};
+  function play() {
+    showLayer();
+    playing = true;
+    const btn = byId('civPlayPauseBtn');
+    btn.classList.remove('icon-play');
+    btn.classList.add('icon-pause');
+  }
+
+  function pause() {
+    playing = false;
+    const btn = byId('civPlayPauseBtn');
+    btn.classList.remove('icon-pause');
+    btn.classList.add('icon-play');
+  }
+
+  function togglePlay() {
+    playing ? pause() : play();
+  }
+
+  function init() {
+    const container = document.getElementById('civPlayerControls');
+    if (!container) return;
+    container.innerHTML = `
+      <button id="civPlayPauseBtn" class="icon-play" data-tip="Play/Pause"></button>
+      <button id="civStepBackBtn" data-tip="Step back">-1</button>
+      <button id="civStepForwardBtn" data-tip="Step forward">+1</button>
+      <span id="civSpeedLabel">Speed:<slider-input id="civSpeed" min="1" max="100" value="50"></slider-input></span>
+      <button id="civReplayBtn" class="icon-cw" data-tip="Replay"></button>
+      <button id="civRegenerateBtn" class="icon-arrows-cw" data-tip="Regenerate"></button>
+    `;
+    container.style.display = 'block';
+    byId('civPlayPauseBtn').on('click', togglePlay);
+  }
+
+  return { init, play, pause, togglePlay };
 })();
 
-document.addEventListener("DOMContentLoaded", () => CivPlayerControls.init());
+document.addEventListener('DOMContentLoaded', () => CivPlayerControls.init());
